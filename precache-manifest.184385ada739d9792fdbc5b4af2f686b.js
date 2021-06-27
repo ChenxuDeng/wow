0c260bc2ea6311054d49476b04b5496689a36f3c
jimmy <836081346@qq.com>
2021-06-27 18:15:39 +0800

    Updates

---
+++ precache-manifest.184385ada739d9792fdbc5b4af2f686b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402781ed61835282a8b3a113851a2353",
+    "url": "/wow/index.html"
+  },
+  {
+    "revision": "1710b9c61578ece7711e",
+    "url": "/wow/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2cab3eedaf032459255",
+    "url": "/wow/static/js/2.c27118a9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wow/static/js/2.c27118a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1710b9c61578ece7711e",
+    "url": "/wow/static/js/main.3fc5d79d.chunk.js"
+  },
+  {
+    "revision": "978cc94ec9cccd089bfc",
+    "url": "/wow/static/js/runtime-main.2b776bc3.js"
+  },
+  {
+    "revision": "a9bca7a3b4eebe3bdb10a839cbbe97ba",
+    "url": "/wow/static/media/background.a9bca7a3.jpg"
+  },
+  {
+    "revision": "495b947de9be4a1616c0ade575793cb8",
+    "url": "/wow/static/media/blizzard.495b947d.svg"
+  },
+  {
+    "revision": "c03b19c521d096bead09b1cbb21f6bb3",
+    "url": "/wow/static/media/blizzardLeft.c03b19c5.svg"
+  },
+  {
+    "revision": "18def74ed60ab9f1f4139f87349d2f79",
+    "url": "/wow/static/media/bottomBackground.18def74e.jpg"
+  },
+  {
+    "revision": "f5f3976cf4be787ad2be202a19d40823",
+    "url": "/wow/static/media/bottomIcon5.f5f3976c.png"
+  },
+  {
+    "revision": "c6933d45ca2d79445786ae90308a66b9",
+    "url": "/wow/static/media/buttonFrame4.c6933d45.jpg"
+  },
+  {
+    "revision": "ff502b26add9da42e3e178a7349958e6",
+    "url": "/wow/static/media/buttonFrame7.ff502b26.jpg"
+  },
+  {
+    "revision": "c50dffe6018eb570b05621ed8cd81ecb",
+    "url": "/wow/static/media/contentBackground.c50dffe6.jpg"
+  },
+  {
+    "revision": "524efe1141dcfcef2405080304ea7c7f",
+    "url": "/wow/static/media/d3.524efe11.png"
+  },
+  {
+    "revision": "166f6ed84862511acfefef2cd5afc681",
+    "url": "/wow/static/media/di.166f6ed8.png"
+  },
+  {
+    "revision": "c51bd2f1f59bad0210b974472a03a61a",
+    "url": "/wow/static/media/esports-hs.c51bd2f1.png"
+  },
+  {
+    "revision": "fcbe0208d6c6f8e95f9f121d9e9a7bb7",
+    "url": "/wow/static/media/esports-ow.fcbe0208.png"
+  },
+  {
+    "revision": "cedb8db0c1417eb68e124d9f362c339b",
+    "url": "/wow/static/media/esports-sc2.cedb8db0.png"
+  },
+  {
+    "revision": "b6e6da3e313449d64aeb35270570fcfb",
+    "url": "/wow/static/media/esports-w3r.b6e6da3e.png"
+  },
+  {
+    "revision": "25e43e9dc0afc45c8dde65dc434e0734",
+    "url": "/wow/static/media/footer.25e43e9d.png"
+  },
+  {
+    "revision": "24c95b6007008dfe5c38eb3da131acf0",
+    "url": "/wow/static/media/heroes.24c95b60.png"
+  },
+  {
+    "revision": "3bdf9384c48c19b02de3591cd1e3cd95",
+    "url": "/wow/static/media/hs.3bdf9384.png"
+  },
+  {
+    "revision": "a5aceeed07718f696e26dc7b9c75f188",
+    "url": "/wow/static/media/leftDrawerButton.a5aceeed.jpg"
+  },
+  {
+    "revision": "cd33662edc379696e73594f1596ed27c",
+    "url": "/wow/static/media/leftDrawerLogo.cd33662e.png"
+  },
+  {
+    "revision": "286c6dae39378b328628a7f3904ae96f",
+    "url": "/wow/static/media/sc2.286c6dae.png"
+  },
+  {
+    "revision": "d08642c803ad2a3b581d23ba86c4d6bc",
+    "url": "/wow/static/media/scr.d08642c8.png"
+  },
+  {
+    "revision": "ab25d13d567013f1430a8d03d6385dec",
+    "url": "/wow/static/media/secondaryNav.ab25d13d.jpg"
+  },
+  {
+    "revision": "8daed474b1da62da00a26b1720663e06",
+    "url": "/wow/static/media/smNavLogo.8daed474.png"
+  },
+  {
+    "revision": "d2566bc99b4990368a84cdfa92368057",
+    "url": "/wow/static/media/topup.d2566bc9.jpg"
+  },
+  {
+    "revision": "a5aceeed07718f696e26dc7b9c75f188",
+    "url": "/wow/static/media/try.a5aceeed.jpg"
+  },
+  {
+    "revision": "90490c403acd7fa411da340029ae9530",
+    "url": "/wow/static/media/w3r.90490c40.png"
+  },
+  {
+    "revision": "6cdd9c9350457dca538b7dfcd540bdd2",
+    "url": "/wow/static/media/wow.6cdd9c93.png"
+  }
+]);
